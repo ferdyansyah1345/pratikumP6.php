d9ec70b9bb6e58fbc8620eb1877b9094f1be3d78
ferdyansyah1345 <ferdysambo1345@gmail.com>
2024-11-02 14:02:28 +0700

    Add files via upload

---
+++ pratikumP6.php
@@ -0,0 +1,146 @@
+<?php
+// PHP Function
+//  function tampilakan_nama(){
+//     echo "Nama saya ucup ngoding";
+
+//  }
+//  tampilakan_nama();
+
+  // string Fuction
+//=======================================
+// $nama = "muhammad raden iqbal hafidz Fauzi ";
+//          echo $nama;
+//          echo '<br/>';
+//          echo strtoupper($nama);
+//          echo '<br/>';
+//          echo ucwords($nama);
+//          echo '<br/>';
+//          echo lcfirst($nama);
+//          echo '<br/>';
+//          echo strtolower($nama);
+
+
+//======================================
+
+// Fungsi Void 
+    // function salam($nama){ 
+    //   echo "<h2>Assalamu'alaikum ".strtoupper($nama)."</h2>";
+    //  }
+    //   salam("ali");
+    //   salam("fadil");
+    //   salam();
+
+  //=======================================
+
+
+    // Fungsi return value 
+    //  function jumlah($a , $b){
+    //    return $a + $b;
+    //  }
+    //    echo  '$a + $b = '.Jumlah(2,8);
+
+    //======================================
+       
+
+    // Membuat Fungsi 
+    // function perkenalan(){
+    //   echo "Assalamu'alaikum,";
+    //   echo "Perkenalkan, nama saya Bedu<br/>";
+    //   echo "Senang berkenalan dengan anda<br/>";
+    // }
+    //   // memanggil fungsi yang sudah di buat 
+    //   perkenalan();
+
+    //   echo "<hr/>";
+
+    //   // memanggil lagi 
+      
+    //   perkenalan();
+     
+    // function void 
+
+    // // memebuat fungsi 
+    // function perkenalan($nama,$salam){
+    //    echo $salam.",";
+    //    echo "  perkenalkan, nama saya ".$nama."<br/>";
+    //    echo " Senang berkenalan dengan anda<br/>";
+    // }
+    //    perkenalan("Usro", "hai");
+
+    //    echo "<hr/>";
+
+    //    $salam = "Bedu";
+    //    $ucapkansalam = "Selamat Pagi";
+
+    //    perkenalan($saya, $ucapkansalam);
+
+    // function perkenalan($nama, $salam="Assalamu'alaikum"){
+    //   echo $salam.",";
+    //   echo "Perkenalan, nama saya ".$nama."<br/>";
+    //   echo "Senang berkenalan dengan anda <br/>";
+    // }
+     
+    //  perkenalan("Jarwo", "Hi");
+
+    //  echo "<hr/>";
+
+    //  $saya = "Bamabang";
+    //  $ucapkansalam = "Saelamat Pagi";
+
+    //  perkenalan($saya);
+  
+        // function hitunganUmur($thn_lahir, $thn_sekarang){
+        //   $umur = $thn_sekarang - $thn_lahir;
+        //   return $umur;
+        // }
+
+        //    echo "Umur saya adalah ".hitunganumur(2003,2022) ." tahun";
+
+          
+          //  function hitunganUmur($thn_lahir, $thn_sekarang){
+          //   $umur = $thn_sekarang - $thn_lahir;
+          //   return $umur;
+          // }
+          //   function perkenalan($nama, $salam="Assalamu'alaikum"){
+          //      echo $salam.",";
+          //      echo "Perkanalkan, nama saya ".$nama."<br/>";
+
+          //      echo "Saya berusisa ". hitungUmur(1994, 2015) ."tahun<br/>";
+          //      echo "Senang berkenalan dengan anda <br/>";
+               
+              
+          //   }
+          //   perkenalan($saya);
+
+
+//           function faktorial($angka) {
+//             if ($angka < 2) {
+//               return 1;  
+//             } else {
+//               return ($angka * faktorial($angka-1));
+//             }
+//           }
+
+//           echo "faktorial 5 adalah ".faktorial(5);
+// 
+ // =======================================
+  // function Variable
+        //  function pangkatDua($a){
+        //   $a = $a * $a;
+
+        //  }
+        //  $a = 20;
+        //  echo 'sebelum nilai $a :'.$a;
+        //  pangkatDua($a);
+        //  echo '<br/>sesudah nilai $a :'.$a;
+
+        // function otentikasi($uname,$pass){
+        //   // user admin, passsword 12345
+        //   if($uname=="admin" && $pass=="12345"){
+        //     return true;
+        //   }else{
+        //     return false;
+        //   }
+        // }
+?>
+
